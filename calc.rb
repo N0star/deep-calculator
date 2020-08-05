ca8b34cebc69b3b205618cb9debb312416885f6a
N0star <64681977+N0star@users.noreply.github.com>
2020-08-05 20:54:52 +0200

    Add files

---
+++ calc.rb
@@ -0,0 +1,195 @@
+### FDC 2020 ###
+require "ruby2d"
+
+$rx = 400; $ry = 600; $bor = 5; $mbr=0;
+set title: "Sophie Calculator"
+set background: 'gray'
+set height: $ry; set width: $rx;
+
+class Kalkulator
+  def initialize
+    @digits = '0'
+    @message = 'subteal subteal subteal subteal subteal subteal subteal'
+    @buttons = []
+    @shift = 0
+    @number = 0
+    @memory = 0
+    @dec = false
+    @sign = ''
+
+    @screen = Rectangle.new(
+      x: $bor, y: $bor,
+      width: $rx-$bor*2,
+      height: $ry / 6,
+      color: 'teal', z: 10
+    )
+
+    @screentext = Text.new(
+      @digits,
+      x: 4*$bor, y: 2*$bor,
+      font: 'data-latin.ttf',
+      size: 72,
+      color: 'black',
+      z: 20
+    )
+
+    @subscreen = Rectangle.new(
+      x: $bor, y: 4*$bor+$ry/6,
+      width: $rx-$bor*2,
+      height: $ry / 12,
+      color: 'teal', z: 10
+    )
+
+    @subtext = Text.new(
+      @message,
+      x: 2*$bor+$rx, y: 4*$bor+$ry/6,
+      font: 'data-latin.ttf',
+      size: 42,
+      color: 'black',
+      z: 20
+    )
+
+    for i in [0,$rx-$bor]
+      Rectangle.new(
+        x:i , y: 0,
+        width: $bor, height: $ry,
+        color: 'gray', z: 30
+      )
+    end
+
+    symbol = ['7','8','9','x','4','5','6','-','1','2','3','+','=','0',',',':']
+
+    for i in 0..3
+      for j in 0..3
+        x = $bor+j*$rx/4
+        y = i*$rx/4+$ry/3
+        button = Button.new(x,y,symbol[j%4+i*4])
+        @buttons.append(button)
+      end
+    end
+  end
+
+  def update
+    @screentext.text=@digits
+    @subtext.text=@message
+    if @shift>$rx*4
+      @shift=0
+    else
+      @shift+=2
+    end
+    @subtext.x=$rx+2*$bor-@shift
+  end
+
+  def action(x,y,a)
+    for i in 0..@buttons.length-1
+      buff=@buttons[i].action(x,y,a)
+      if a=='perform'
+        if not buff.nil?
+          symbol=buff
+          f=true if Integer(symbol) rescue f=false
+          if f
+            @message=@message+symbol
+            if @number==0
+              @digits=symbol
+            elsif @digits.length<11
+              @digits=@digits+symbol
+            end
+            @number=Float(@digits)
+          elsif symbol==','
+            @dec=true
+          else
+            @dec=false
+            if(symbol=='=')
+              case @sign
+              when '+'
+                @number=@number+@memory
+              when '-'
+                @number=@memory-@number
+              when 'x'
+                @number=@memory*@number
+              when ':'
+                @number=@memory/@number
+              end
+              @digits=String(@nymber)
+            else
+              @sign=symbol
+              @memory=@number
+              @number = 0
+              @digits = '0'
+            end
+          end
+          break;
+        end
+      end
+    end
+  end
+
+  def reset
+    for i in 0..@buttons.length-1
+      @buttons[i].reset
+    end
+  end
+end
+
+class Button
+  def initialize(x,y,symbol)
+    @symbol=symbol
+    @x=x; @y=y;
+    @sq = Square.new(
+      x: x, y: y,
+      size: $rx/4-$bor,
+      color: 'black',
+      z: 10
+    )
+    if @symbol=='='
+      @sq.color='red'
+    end
+    @tag = Text.new(
+      @symbol,
+      x: x+$bor*2, y: y+$bor,
+      font: 'data-latin.ttf',
+      size: 36, color: 'white',
+      z: 20
+    )
+    @color=@sq.color
+  end
+
+  def action(x,y,a)
+    if @sq.x<x and x<@sq.x+@sq.size and @sq.y<y and y<@sq.y+@sq.size
+      case a
+      when 'click'
+          @sq.color='silver'
+      when 'perform'
+        return @symbol
+      end
+    end
+  end
+
+  def reset
+    @sq.color=@color
+  end
+end
+
+
+kalk=Kalkulator.new
+zegar = 0
+update do
+  if zegar % 1 == 0
+    kalk.update
+  end
+    on :mouse_down do |event|
+      if $mbr==0
+        kalk.action(event.x,event.y,'click')
+        kalk.action(event.x,event.y,'perform')
+        $mbr=1
+      end
+    end
+    on :mouse_up do
+      kalk.reset; $mbr=0;
+    end
+    #puts event
+  #end
+  zegar += 1
+end
+
+show
